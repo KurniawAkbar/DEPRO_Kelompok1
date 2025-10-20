9e6f944b72c41e01bda6fcc7ee148ec6088856df
femiad <femiadwi696@gmail.com>
2025-10-20 11:22:31 +0700

    Upload my project

---
+++ main.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+void hitungTotal(int jumlah) {
+    float resistor, total = 0;
+
+    for (int i = 1; i <= jumlah; i++) {
+        printf("Masukkan nilai resistor ke-%d (Ohm): ", i);
+        scanf("%f", &resistor);
+        total = total + resistor;
+    }
+
+    printf("Total hambatan = %.2f Ohm\n", total);
+}
+int main() {
+    int jumlahResistor;
+
+    printf("Masukkan jumlah resistor yang akan dihitung: ");
+    scanf("%d", &jumlahResistor);
+
+    hitungTotal(jumlahResistor);
+
+    return 0;
+}
